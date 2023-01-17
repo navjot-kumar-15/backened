9103802942c9bd9d4de2bdcc5c6b7b37215a1c69
navjot-kumar-15 <110272866+navjot-kumar-15@users.noreply.github.com>
2023-01-17 11:49:54 +0530

    authtoken done

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const uniqueValidator = require("mongoose-unique-validator");
+// const uniqueValidator = require("mongoose-unique-validator");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -20,5 +20,5 @@ const UserSchema = new mongoose.Schema({
     default: Date.now,
   },
 });
-UserSchema.plugin(uniqueValidator);
+// UserSchema.plugin(uniqueValidator);
 module.exports = mongoose.model("Users", UserSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -2,33 +2,58 @@ const express = require("express");
 const router = express.Router();
 const user = require("../models/User");
 const { body, validationResult } = require("express-validator");
+const { exists } = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "navjotisagoodb$oy";
 
-// CREATING A USER USING : POST "api/auth". DOES'NT REQUIRE AUTH
+// Creating a user or saving the user by using : POST -> method  "api/auth/createuser".No loign require.
 router.post(
-  "/",
+  "/createuser",
+  // Validator using
   [
     body("name", "Enter the valid name").isLength({ min: 3 }),
     body("email", "Please check your email").isEmail(),
     body("password").isLength(5),
   ],
-  (req, res) => {
+  async (req, res) => {
+    // If there are error then shows bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    user
-      .create({
+    // Check whether the user already exist or not
+    // Handling error
+    try {
+      let User = await user.findOne({ email: req.body.email });
+      // Showing the error if user already exists
+      if (User) {
+        return res
+          .status(400)
+          .json({ error: "Sorry with this email user already exist" });
+      }
+      // Hashing the password using bcrypt js
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+      // Creation of user
+      User = await user.create({
         name: req.body.name,
-        password: req.body.password,
+        password: secPass,
         email: req.body.email,
-      })
-      .then((user) => {
-        res.json(user);
-      })
-      .catch((e) => {
-        console.log(e);
-        res.json({ error: "Please enter the valid email" });
       });
+      // Applying JWT authentication below
+      const data = {
+        User: {
+          id: User.id,
+        },
+      };
+      const jwtAuthToken = jwt.sign(data, JWT_SECRET);
+      console.log(jwtAuthToken);
+      res.json({ jwtAuthToken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Some error occured");
+    }
   }
 );
 module.exports = router;
